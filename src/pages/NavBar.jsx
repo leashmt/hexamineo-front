fb0da6c08071d13456262e0e04f3f6ae333faf79
Lisa HURTAUD <lisa.hurtaud@supdevinci-edu.fr>
2024-12-17 09:15:09 +0100

    feat(homepage) : develop homepage

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,19 +1,29 @@
 import { Link, Outlet } from "react-router-dom";
+import footerImage from "../imgs/footer-little-prince.png";
 
 const NavBar = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center text-black relative">
-      <header className="absolute top-0 w-full py-4 flex justify-center">
-        <nav className="flex space-x-6 text-lg">
-          <Link to="/" className="hover:underline">
-            Accueil
-          </Link>
-          <Link to="/example" className="hover:underline">
-            Example
-          </Link>
+    <div className="bg-white-background min-h-screen flex flex-col">
+      <header className="py-4 shadow-lg">
+        <nav className="flex justify-center space-x-6 text-lg">
+          <div className = "flex space-x-8">   
+            <Link to="/" className="hover:text-yellow-highlight">Accueil</Link>
+            <Link to="/classes" className="hover:text-yellow-highlight">Visualiser les classes</Link>
+            <Link to="/auth" className="hover:text-yellow-highlight">Login/Register</Link>
+          </div>
         </nav>
       </header>
-      <Outlet />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <div className="w-full">
+        <img src={footerImage} alt="Silhouette du petit prince" className="w-full object-cover"/>
+      </div>
+      <footer className="w-full bg-purple-custom text-white py-4">
+        <div className="container mx-auto">
+          <p>@2024 Hexamineo. Tous droits réservés.</p>
+        </div>
+      </footer>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-custom": "#5F6C96",
+        "white-background": "#F4F6FA",
+        "yellow-highlight": "#FFD166"
+      }
+    },
   },
   plugins: [],
 }
