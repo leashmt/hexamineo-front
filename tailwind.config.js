2e4e63907d6d76f68540a582a79b5e1ffde176c3
leaschmt <lea.schmitt@supdevinci-edu.fr>
2024-12-17 14:49:14 +0100

    fix(tailwind): update AddStudent component and add Tailwind CSS dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-/**  @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src//*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "purple-custom": "#5F6C96",
-        "white-background": "#F4F6FA",
-        "yellow-highlight": "#FFD166"
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				'purple-custom': '#5F6C96',
+				'white-background': '#F4F6FA',
+				'yellow-highlight': '#FFD166',
+			},
+		},
+	},
+	plugins: [],
+};
