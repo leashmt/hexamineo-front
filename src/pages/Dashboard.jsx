1c810dd8e145f8df2a97d859849c9b0440e73543
bilal <bilal-rockstar@outlook.com>
2024-12-19 10:27:06 +0100

    Style(Dashboard) : fix style header and footer in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -47,7 +47,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="p-8">
       <h1 className="text-3xl font-bold mb-6 text-gray-800">Dashboard</h1>
 
       <div className="bg-white p-6 rounded-lg shadow-md mb-6">
